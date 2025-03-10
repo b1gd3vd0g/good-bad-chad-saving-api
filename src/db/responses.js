8726ad25cc3f0f30965097f8c51a38b2d368dd83
b1gd3vd0g <devin@bigdevdog.com>
2025-03-09 17:50:39 -0700

    delete files from base folder (its now two separate apps)

--- src/db/responses.js
+++
@@ -1,9 +0,0 @@
-/** Returns a formatted CrudResponse object for easy use with routers. */
-const crudResponse = (code = 200, info = {}) => {
-    if (typeof info === 'string') {
-        info = { message: info };
-    }
-    return { code, info };
-};
-
-module.exports = crudResponse;
