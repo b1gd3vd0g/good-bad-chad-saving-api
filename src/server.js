3122d1fc778709637919122b2dc8f63d125273db
b1gd3vd0g <devin.peevy@outlook.com>
2025-06-26 16:34:09 -0700

    remove reference to dotenv dependency. Remove HTTPS configuration from the app (it will be handled externally).

--- src/server.js
+++ src/server.js
@@ -1,17 +1,4 @@
-require('dotenv').config();
-
 const app = require('./app');
-const fs = require('fs');
-
-const { CERT_PATH, KEY_PATH } = process.env;
-
-const options = {
-    cert: fs.readFileSync(CERT_PATH),
-    key: fs.readFileSync(KEY_PATH)
-};
-
-const server = require('https').createServer(options, app);
-
-server.listen(6900, () => {
-    console.log('Server listening on port 6900');
-});
+const server = require('http').createServer(app);
+const port = 6900;
+server.listen(port, () => console.log(`Server listening on port ${port}.`));
