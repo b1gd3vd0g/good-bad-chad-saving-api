fadd3c1c57aec0f4493c77270358a4e724a34d67
b1gd3vd0g <devin.peevy@outlook.com>
2025-06-26 17:13:03 -0700

    Update to require FEHOST environment variable (the domain that the frontend game is hosted at). Configure CORS to be strict and only to allow requests from the frontend.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 
+const { FEHOST } = process.env;
+if (!FEHOST) {
+  throw new Error('ENVIRONMENT VARIABLE IS NOT CONFIGURED TO SET UP CORS.');
+}
+
 const app = express();
 
 app.use(
-    cors({
-        credentials: true,
-        origin: '*',
-        allowedHeaders: '*'
-    })
+  cors({
+    credentials: true,
+    origin: [`https://${FEHOST}`, `https://www.${FEHOST}`],
+    methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    maxAge: 3600
+  })
 );
 
 app.use(express.json());
